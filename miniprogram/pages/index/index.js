c3daf1e6e657ad53b5c3c0030ec1fbd2f62d5fc1
wlt <320523743@qq.com>
2020-10-28 21:02:29 +0800

    教室第二次提交

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,20 +6,32 @@ const app = getApp()
 Page({
   data: {
     swiper:[],
-
+    content_swiper:[],
+    city:['郑州','洛阳','开封','邯郸','成都','上海','东京','重庆','更多'],
   },
 
   onLoad: function() {
 
     //获取顶部轮播图片
     db.collection('indexs').where({
-      _id: "swiper"
+      name: "swiper"
     }).get().then(res=> {
-      console.log(res.data[0].imgs)
+      // console.log(res.data);
       this.setData({
-        swiper:this.data.swiper.concat(res.data[0].imgs)
+        swiper:this.data.swiper.concat(res.data)
       })
     })
+    // index第二个轮播
+    db.collection('indexs').where({
+      name: "content_swiper"
+    }).get().then(res=> {
+      console.log(res.data);
+      this.setData({
+        content_swiper:this.data.content_swiper.concat(res.data)
+      })
+    })
+  
+
   },
 
 })
