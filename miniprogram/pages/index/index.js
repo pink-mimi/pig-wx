7863601deb38e5456af5562ecc0e23197d92568e
王多鱼 <320523743@qq.com>
2020-10-29 08:03:30 +0800

    寝室第一次提交

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,6 +8,8 @@ Page({
     swiper:[],
     content_swiper:[],
     city:['郑州','洛阳','开封','邯郸','成都','上海','东京','重庆','更多'],
+    num:0,
+    house:[]
   },
 
   onLoad: function() {
@@ -25,13 +27,33 @@ Page({
     db.collection('indexs').where({
       name: "content_swiper"
     }).get().then(res=> {
-      console.log(res.data);
+      // console.log(res.data);
       this.setData({
         content_swiper:this.data.content_swiper.concat(res.data)
+      })
+    })
+
+    // goods房子
+    db.collection('indexs').where({
+      name: "goods"
+    }).get().then(res=> {
+      console.log(res.data,111);
+      this.setData({
+        house:this.data.house.concat(res.data)
       })
     })
   
 
   },
+  highlight(e){
+    this.setData({
+      num:e.currentTarget.dataset.index
+    })
+  },
+  more(){
+    wx.navigateTo({
+      url: '../more/more',
+    })
+  }
 
 })
